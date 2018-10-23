626ce00bc1044fb78e795bd3577f131fbd98d011
nik <nikdiculous@gmail.com>
2018-10-23 15:51:42 +1100

    finally merges and solve merging bugs

--- app/src/main/java/com/example/willi/buddy/toDo.java
+++ app/src/main/java/com/example/willi/buddy/toDo.java
@@ -1,18 +1,44 @@
 package com.example.willi.buddy;
 
-public class toDo {
-    private String task;
+import android.content.Intent;
+import android.support.annotation.NonNull;
+import android.support.design.widget.BottomNavigationView;
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.view.MenuItem;
 
-    public toDo() {
+public class Todo extends AppCompatActivity {
 
+    @Override
+    public void onBackPressed() {
+        super.onBackPressed();
+        startActivity(new Intent(Todo.this, MainActivity.class));
     }
 
-    public String getToDo() {
-        return task;
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_todo);
+
+        BottomNavigationView navigation = (BottomNavigationView) findViewById(R.id.navigation);
+        navigation.setOnNavigationItemSelectedListener(mOnNavigationItemSelectedListener);
+        navigation.setSelectedItemId(R.id.navigation_todo);
     }
 
-    public void setToDo(String task) {
-        this.task = task;
-    }
+    private BottomNavigationView.OnNavigationItemSelectedListener mOnNavigationItemSelectedListener
+            = new BottomNavigationView.OnNavigationItemSelectedListener() {
 
+        @Override
+        public boolean onNavigationItemSelected(@NonNull MenuItem item) {
+            switch (item.getItemId()) {
+                case R.id.navigation_Quiz:
+                    startActivity(new Intent(Todo.this, quiz_home.class));
+                    return true;
+                case R.id.navigation_todo:
+                    //mTextMessage.setText(R.string.title_Todo);
+                    return true;
+            }
+            return false;
+        }
+    };
 }
